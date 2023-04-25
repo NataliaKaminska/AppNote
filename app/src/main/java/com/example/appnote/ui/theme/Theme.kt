e7aef4e295336072f86d267b66236a20aaa5af65
NataliaKaminska <kaminskanatalia037@gmail.com>
2023-04-25 10:24:30 +0200

    update: dark theme

--- app/src/main/java/com/example/appnote/ui/theme/Theme.kt
+++ app/src/main/java/com/example/appnote/ui/theme/Theme.kt
@@ -10,15 +10,21 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+//    primary = Purple80,
+//    secondary = PurpleGrey80,
+//    tertiary = Pink80
+    primary = Color(0xFFBB86FC),
+    background = Color(0xFF121212),
+    onBackground = Color.White,
+    surface = Color(0xFF333333),
+    onSurface = DarkGray
 )
 
 private val LightColorScheme = lightColorScheme(
